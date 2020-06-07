2f7dbce3064626c1b9446f92945abd09d80fde2e
Ryan S <kobitoko@gmail.com>
2020-06-06 21:15:34 -0600

    Initial works on client and host.

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
+# Remote Mouse Control
+
+
+
+Build ontop of TypeScript Node Starter taken from https://github.com/Microsoft/TypeScript-Node-Starter
+
+<details><summary>TypeScript Node Starter Readme</summary>
+
 # TypeScript Node Starter
 
 The main purpose of this repository is to show a working Node.js API Server + front-end project and workflow for writing Node code in TypeScript.
 
 It is not a goal to be a comprehensive and definitive guide to making a TypeScript and Node project, but as a working reference maintained by the community. If you are interested in starting a new TypeScript project - check out the bootstrapping tools reference in [the TypeScript Website](https://www.typescriptlang.org/docs/home.html)
-
-
-[![Dependency Status](https://david-dm.org/Microsoft/TypeScript-Node-Starter.svg)](https://david-dm.org/Microsoft/TypeScript-Node-Starter) [![Build Status](https://travis-ci.org/Microsoft/TypeScript-Node-Starter.svg?branch=master)](https://travis-ci.org/Microsoft/TypeScript-Node-Starter)
-
-**Live Demo**: [https://typescript-node-starter.azurewebsites.net/](https://typescript-node-starter.azurewebsites.net/)
-
-![image](https://user-images.githubusercontent.com/820883/36764267-abbdb7f8-1be0-11e8-9678-2a9ea448d7f8.png)
 
 
 # Table of contents:
@@ -557,3 +558,4 @@ A majority of this quick start's content was inspired or adapted from Sahat's ex
 ## License
 Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the [MIT](LICENSE.txt) License.
+</details>
--- src/public/js/main.ts
+++ src/public/js/main.ts
@@ -1,3 +1,78 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    // Place code here...
-})
+    const remoteMouse = new RemoteMouse();
+    const startBtn = document.getElementById("start-client");
+    const stopBtn = document.getElementById("stop");
+    startBtn.addEventListener("click", () => remoteMouse.startCapture());
+    stopBtn.addEventListener("click", () => remoteMouse.stopCapture());
+    console.log("ready");
+});
+
+type DisplayMediaOptions = {
+    video: { cursor: string };
+    audio: boolean;
+};
+
+class RemoteMouse {
+    public displayMediaOptions: DisplayMediaOptions = null;
+    private captureStream: any = null;
+
+    constructor(displayMediaOptions?: DisplayMediaOptions) {
+        this.displayMediaOptions = displayMediaOptions
+            ? displayMediaOptions
+            : {
+                  video: {
+                      cursor: "always",
+                  },
+                  audio: false,
+              };
+        console.log("Constructed!");
+    }
+
+    public handleMouseMoved(e: MouseEvent) {
+        console.log(
+            "Mouse move",
+            e.offsetX,
+            e.offsetY,
+            (e.target as HTMLElement).clientWidth,
+            (e.target as HTMLElement).clientHeight,
+            e.offsetX / (e.target as HTMLElement).clientWidth,
+            e.offsetY / (e.target as HTMLElement).clientHeight
+        );
+    }
+
+    public handleMouseDown(e: MouseEvent) {
+        console.log("Mouse down", e.offsetX, e.offsetY);
+    }
+
+    public handleMouseUp(e: MouseEvent) {
+        console.log("Mouse up", e.offsetX, e.offsetY);
+    }
+
+    public async startCapture(): Promise<void> {
+        try {
+            // navigator.mediaDevices is missing getDisplayMedia: https://github.com/microsoft/TypeScript/issues/33232
+            // @ts-ignore
+            this.captureStream = await navigator.mediaDevices.getDisplayMedia(this.displayMediaOptions);
+            const videoElem: HTMLVideoElement = document.getElementById("video") as HTMLVideoElement;
+            videoElem.srcObject = this.captureStream;
+            await videoElem.requestFullscreen();
+            await videoElem.play();
+            videoElem.addEventListener("mousemove", this.handleMouseMoved);
+            videoElem.addEventListener("mousedown", this.handleMouseDown);
+            videoElem.addEventListener("mouseup", this.handleMouseUp);
+        } catch (err) {
+            console.error("Error: " + err);
+        }
+        console.log(this.captureStream);
+    }
+
+    public stopCapture(): void {
+        const videoElem: HTMLVideoElement = document.getElementById("video") as HTMLVideoElement;
+        let tracks = (videoElem.srcObject as MediaStream).getTracks();
+        tracks.forEach((track) => track.stop());
+        videoElem.srcObject = null;
+        videoElem.removeEventListener("mousemove", this.handleMouseMoved);
+        videoElem.removeEventListener("mousedown", this.handleMouseDown);
+        videoElem.removeEventListener("mouseup", this.handleMouseUp);
+    }
+}
